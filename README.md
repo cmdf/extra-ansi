8aa1bbbf324cc74293d6131dd10b11f90e34bbbe
Subhajit Sahu <wolfram77@gmail.com>
2016-08-15 14:02:43 +0530

    readme: created

---
+++ README.md
@@ -0,0 +1,18 @@
+# oansi
+
+Process ANSI escape codes in Windows Console.
+
+
+## usage
+
+```batch
+> oansi
+
+:: [] -> optional argument
+:: <> -> argument value
+```
+
+```batch
+:: view blocktronics_baud_dudes ansi art from http://sixteencolors.net/
+> type h7-24lines4bd.ans | oansi
+```
--- cmd-ansi/Program.cs
+++ cmd-ansi/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.IO;
 using orez.oansi.esc;
 
 namespace orez.oansi {
 	class Program {
 		static void Main(string[] args) {
-			// args = new string[] { "h7-24lines4bd.ans" };
-			// Console.SetIn(new StreamReader(args[0]));
 			int a = 0;
 			while(a >= 0)
 				a = oC0.Write(null);
